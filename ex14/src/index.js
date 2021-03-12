3a6c5ac8c9445710467c3cfa9b228fddb2d789a4
haris <haris_aksalic@hotmail.com>
2021-03-12 21:24:52 +0100

    Adding index.js

---
+++ ex14/src/index.js
@@ -0,0 +1,23 @@
+function main() {
+  var myNoun = "dog";
+  var myVerb = "ran";
+  var myAdjective = "big";
+  var myAdverb = "quickly";
+  var wordBlanks =
+    "My " +
+    myNoun +
+    " can " +
+    myVerb +
+    " as fast as Bolt" +
+    ". " +
+    "He is " +
+    myAdjective +
+    " and he is growing " +
+    myAdverb +
+    ".";
+
+  return wordBlanks;
+}
+
+console.log(main());
+module.exports = main;
